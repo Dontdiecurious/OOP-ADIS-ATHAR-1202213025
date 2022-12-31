434041c7c5d23e17617534373b20df4ec8ed86d4
Athar <89459633+Dontdiecurious@users.noreply.github.com>
2022-12-31 13:40:46 +0700

    Add files via upload

---
+++ MODUL3_ATHAR/Calculation.java
@@ -0,0 +1,39 @@
+public class Calculation {
+
+    public static void main(String[] args) {
+        double radius, side, area;
+        double phi = 3.14;
+
+    }
+
+    public static setSquare() {
+
+
+    }
+
+    public static getSquare() {
+        LuasSquare
+    }
+
+    public static setCircle() {
+
+
+
+    }
+
+    public static getCircle() {
+
+    }
+
+    public static setTrapezoid() {
+
+
+    }
+
+    public static getTrapezoid() {
+
+    }
+
+
+
+}
---
+++ MODUL3_ATHAR/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean repeat = true;
+
+    do {
+
+        System.out.println(""" 
+        ===Menu Program===
+        1. Square
+        2. Circle
+        3. Trapeszoid""");
+
+        int LuasSquare, LuasCircle, LuasTrapezoid;
+        try {
+
+            switch (){
+                case 1:
+                System.out.print("Input luas square: ");
+                LuasSquare = input.nextInt();
+                break;
+
+                case 2:
+                System.out.print("Input luas circle: ");
+                LuasCircle = input.nextInt();
+                break;
+
+                case 3:
+                System.out.print("Input luas Trapezoid: ");
+                LuasTrapezoid = input.nextInt();
+                break;
+
+                case 0:
+                System.out.println("Program End");
+                
+                default:
+                System.out.println("Option not available");
+
+            }
+
+            repeat =
+        }
+
+    }
+
+    
+}
